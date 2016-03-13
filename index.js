386f4519bc86cffa28d8984b0274c73d97f3058b
Nick Stefan <NickStefan12@gmail.com>
2016-03-13 15:40:17 -0700

    multi $lookup works, but multi $unwind not going well

--- index.js
+++ index.js
@@ -40,61 +40,139 @@ async.auto({
     ], done);
   }],
 
-  // QUERY THE IMAGES FOR THE IDs
-  queryImages: ['insertImages', function(done, results){
+  // INSERT SOME TEXT
+  insertText: ['insertImages', function(done, results){
     results.db.collection('content')
-    .find()
+    .insertMany([
+      {
+        type: 'Text',
+        text: 'Stock photos look nice.',
+      },
+      {
+        type: 'Text',
+        text: 'Is there such a thing as stock text?'
+      },
+      {
+        type: 'Text',
+        text: 'Lorem Ipsum isn\'t really stock text'
+      }
+    ], done);
+  }],
+
+  // QUERY THE IMAGES FOR THE IDs
+  queryImages: ['insertText', function(done, results){
+    results.db.collection('content')
+    .find({type:'Image'})
+    .toArray(done);
+  }],
+
+  // QUERY THE TEXT FOR THE IDs
+  queryText: ['queryImages', function(done, results){
+    results.db.collection('content')
+    .find({type: 'Text'})
     .toArray(done);
   }],
 
   // CREATE A PAGE WITH SOME IMAGE IDs
-  insertPage: ['queryImages', function(done, results){
-    var parent = new ObjectID();
-    var leftChild = new ObjectID();
-    var middleChild = new ObjectID();
-    var rightChild = new ObjectID();
+  insertPage: ['queryText', function(done, results){
+    var root = new ObjectID();
+
+    var sectionOne = new ObjectID();
+    var sectionTwo = new ObjectID();
+
+    var sectionOneLeftChild = new ObjectID();
+    var sectionOneMiddleChild = new ObjectID();
+    var sectionOneRightChild = new ObjectID();
+
+    var sectionTwoLeftChild = new ObjectID();
+    var sectionTwoMiddleChild = new ObjectID();
+    var sectionTwoRightChild = new ObjectID();
 
     results.db.collection('content')
     .insertOne({
       type: 'Page',
       nodes: [
         {
-          _id: parent,
+          _id: root,
+          parent_id: null,
+          next_id: null,
+          previous_id: null
+        },
+        {
+          _id: sectionOne,
           parent_id: null,
           next_id: null,
           previous_id: null,
           view: 'section'
         },
         {
-          _id: leftChild,
+          _id: sectionTwo,
+          parent_id: null,
+          next_id: null,
           previous_id: null,
-          next_id: middleChild,
-          parent_id: parent,
+          view: 'section'
+        },
+        {
+          _id: sectionOneLeftChild,
+          previous_id: null,
+          next_id: sectionOneMiddleChild,
+          parent_id: sectionOne,
           view: 'image',
           props: {
             image_id: results.queryImages[0]._id
           }
         },
         {
-          _id: middleChild,
-          previous_id: leftChild,
-          next_id: rightChild,
-          parent_id: parent,
+          _id: sectionOneMiddleChild,
+          previous_id: sectionOneLeftChild,
+          next_id: sectionOneRightChild,
+          parent_id: sectionOne,
           view: 'image',
           props: {
             image_id: results.queryImages[1]._id
           }
         },
         {
-          _id: rightChild,
-          previous_id: middleChild,
+          _id: sectionOneRightChild,
+          previous_id: sectionOneMiddleChild,
           next_id: null,
-          parent_id: parent,
+          parent_id: sectionOne,
           view: 'image',
           props: {
             image_id: results.queryImages[2]._id
           }
+        },
+                {
+          _id: sectionTwoLeftChild,
+          previous_id: null,
+          next_id: sectionTwoMiddleChild,
+          parent_id: sectionTwo,
+          view: 'text',
+          props: {
+            text_id: results.queryText[0]._id
+          }
+        },
+        {
+          _id: sectionTwoMiddleChild,
+          previous_id: sectionTwoLeftChild,
+          next_id: sectionTwoRightChild,
+          parent_id: sectionTwo,
+          view: 'text',
+          props: {
+            text_id: results.queryText[1]._id
+          }
+        },
+        {
+          _id: sectionTwoRightChild,
+          previous_id: sectionTwoMiddleChild,
+          next_id: null,
+          parent_id: sectionTwo,
+          view: 'text',
+          props: {
+            text_id: results.queryText[2]._id
+          }
         }
+
       ]
     }, done);
   }],
@@ -119,8 +197,19 @@ async.auto({
         }
       },
       {
-        '$unwind': '$nodes.props.image'
+        '$lookup':{
+          from: 'content',
+          localField: 'nodes.props.text_id',
+          foreignField: '_id',
+          as: 'nodes.props.text'
+        }
       },
+      // {
+      //   '$unwind': '$nodes.props.image'
+      // },
+      // {
+      //   '$unwind': '$nodes.props.text'
+      // },
       {
         '$group': {
           _id: '$_id',
